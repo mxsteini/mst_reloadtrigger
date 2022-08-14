16193c37392a11843d324f8c6974c7749b235c76
Michael Stein <info@michaelstein-itb.de>
2022-08-14 23:16:02 +0200

    remove nonsense

--- ext_tables.php
+++ ext_tables.php
@@ -4,8 +4,4 @@ defined('TYPO3_MODE') || exit();
 
 (static function (): void {
     $extensionKey = 'mst_contentfallback';
-
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Domain\Repository\PageRepository::class] = [
-        'className' => MST\MstContentfallback\XClass\PageRepository::class
-    ];
 })();
